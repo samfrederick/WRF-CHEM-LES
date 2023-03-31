5f7dd8731b5a2db9e30d5da772074cbafc8e495a
Frederick <sf20@keeling.earth.illinois.edu>
2023-03-31 08:51:20 -0500

    Change output directory path to archive directory at nriemer/d/sf20. Move rst files also.

--- create_move_output_files.py
+++ create_move_output_files.py
@@ -4,20 +4,23 @@ import shutil
 import fnmatch
 
 wrk_dir = r"/data/keeling/a/sf20/b/WRF4_4/WRF/test/em_les"
+output_dir = r"/data/nriemer/d/sf20/les_output"
 now = datetime.now()
 datestamp = now.strftime('%Y-%m-%d')
 timestamp = datetime.now().strftime('%H%M%S')
-output_path = os.path.join(wrk_dir, datestamp, timestamp)
+output_path = os.path.join(output_dir, datestamp, timestamp)
 if not os.path.isdir(output_path):
     os.makedirs(output_path)
+    print(f'{output_path}')
 
 error_files = fnmatch.filter(os.listdir('.'), "rsl.error.*")
 out_files = fnmatch.filter(os.listdir('.'), "rsl.out.*")
 slurm_files = fnmatch.filter(os.listdir('.'), "slurm-*.out*")
 wrfinput_files = fnmatch.filter(os.listdir('.'), "wrfinput_*")
 wrfoutput_files = fnmatch.filter(os.listdir('.'), "wrfout_*")
+wrfrst_files = fnmatch.filter(os.listdir('.'), "wrfrst_*")
 
-move_files = error_files + out_files + slurm_files + wrfinput_files + wrfoutput_files
+move_files = error_files + out_files + slurm_files + wrfinput_files + wrfoutput_files + wrfrst_files
 
 for file in move_files:
     shutil.move(f"{wrk_dir}/{file}", f"{output_path}/{file}")
