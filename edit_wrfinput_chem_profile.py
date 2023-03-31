68e0feff04eb2f95df0504a44d616d3d2cbab7df
Frederick <sf20@keeling.earth.illinois.edu>
2023-03-31 10:17:03 -0500

    Move emissions profiles into separate python script for greater modularity

--- edit_wrfinput_chem_profile.py
+++ edit_wrfinput_chem_profile.py
@@ -4,6 +4,7 @@ import os, sys
 import numpy as np
 from scipy.stats import multivariate_normal
 from netCDF4 import Dataset
+from emiss_profiles import gauss3d_profile, checkerboard_profile
 
 try:
     profile_type = sys.argv[1]
@@ -16,86 +17,26 @@ except IndexError:
     print ("Tracer species not specified!")
     sys.exit()
 
-# MESH VARIABLES
-xgrid = 100
-ygrid = 100
-zgrid = 200
-
-def create_mesh_grid():
-    xmax = xgrid - 1
-    ymax = ygrid - 1
-    zmax = zgrid - 1
-    x, y, z = np.mgrid[0:1:(xmax*1j), 0:1:(ymax*1j), 0:1:(zmax*1j)]
-
-    return x, y, z
-
-def gauss3d_profile():
-    x, y, z = create_mesh_grid()
-    # Need an (N, 2) array of (x, y) pairs.
-    xyz = np.column_stack([x.flat, y.flat, z.flat])
-
-    mu = np.array([.5, .5, 0])
-
-    sigma = np.array([.25, .25, .15])
-    covariance = np.diag(sigma**2)
-
-    val = multivariate_normal.pdf(xyz, mean=mu, cov=covariance)
-
-    # Reshape back to a (30, 30) grid.
-    val = val.reshape(x.shape)
-
-    # Normalize max to 100 (ppmv) and transpose so orientation is correct (z up)
-    val = val.T*(100/val.max())
-
-    # Assign minimum value via ambient concentration populated in array by WRF-Chem
-    # prior to modification
-    val[val<0.03] = 0.03
-
-    print(val.shape)
-
-    new_o3_vals = np.ma.array(np.array([val]), mask=False, dtype='float32')
-
-    return new_o3_vals
-
-def checkerboard_profile():
-    xrange, yrange = xgrid-1, ygrid-1
-    epsilon = 0.001
-    Ax, Ay = 1, 1
-    fx, fy = 2, 2
-    print(f'\nUsing fx={fx} and fy={fy}\n')
-    k = fx*2*np.pi/xrange #wavenumber 2pi / L
-    m = fy*2*np.pi/yrange
-    x=np.arange(xrange)
-    y=np.arange(yrange)
-    X,Y=np.meshgrid(x,y)
-    phi=Ax*np.sin(k*X+epsilon)*Ay*np.sin(m*Y+epsilon)
-
-    phi_star = phi.copy()
-
-    max_conc = 100 # ppmv 
-    min_conc = 0.03 #ppmv 
-
-    phi_star[phi_star > 0] = max_conc
-    phi_star[phi_star <= 0] = min_conc
-
-    mesh = np.zeros((xgrid-1, ygrid-1, zgrid-1))
-    mesh[:, :, 0] = phi_star
-
-    # transpose so z, y, x
-    mesh = mesh.T
-
-    mesh = np.ma.array(np.array([mesh]), mask=False, dtype='float32')
-
-    return mesh
-
 def create_modified_netcdf():
     # Code via 
     # https://stackoverflow.com/questions/15141563/python-netcdf-making-a-copy-of-all-variables-and-attributes-but-one
     # User Rich Signell
 
     # Modified by Sam Frederick, October 2022
+
+    # Checkerboard profile variables
+    fx = 1
+    fy = 1
+    min_val = 0.01
+    max_val = 100
+
+    # MESH VARIABLES
+    xgrid = 99
+    ygrid = 99
+    zgrid = 199
     
-    tracer_profile = profile_func()
+    tracer_profile = profile_func(fx=fx, fy=fy, xgrid=xgrid, ygrid=ygrid, 
+                                  zgrid=zgrid, min_val=min_val, max_val=max_val)
 
     toexclude = [species]
 
@@ -108,14 +49,16 @@ def create_modified_netcdf():
     for name, dimension in src.dimensions.items():
         dst.createDimension(
             name, (len(dimension) if not dimension.isunlimited() else None))
-    # copy all file data except for the excluded
+    
     for name, variable in src.variables.items():
+        # copy all file data except for the excluded
         if name not in toexclude:
             x = dst.createVariable(name, variable.datatype, variable.dimensions)
             dst[name][:] = src[name][:]
             # copy variable attributes all at once via dictionary
             dst[name].setncatts(src[name].__dict__)
         else:
+            # for excluded variable modify the variable data values with tracer profile
             x = dst.createVariable(name, variable.datatype, variable.dimensions)
             dst[name][:] = tracer_profile
             # copy variable attributes all at once via dictionary
---
+++ emiss_profiles.py
@@ -0,0 +1,66 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+
+def create_mesh_grid(xgrid=100, ygrid=100, zgrid=200):
+    xmax = xgrid - 1
+    ymax = ygrid - 1
+    zmax = zgrid - 1
+    x, y, z = np.mgrid[0:1:(xmax*1j), 0:1:(ymax*1j), 0:1:(zmax*1j)]
+
+    return x, y, z
+
+def gauss3d_profile(xgrid=100, ygrid=100, zgrid=200):
+    x, y, z = create_mesh_grid(xgrid, ygrid, zgrid)
+    # Need an (N, 2) array of (x, y) pairs.
+    xyz = np.column_stack([x.flat, y.flat, z.flat])
+
+    mu = np.array([.5, .5, 0])
+
+    sigma = np.array([.25, .25, .15])
+    covariance = np.diag(sigma**2)
+
+    val = multivariate_normal.pdf(xyz, mean=mu, cov=covariance)
+
+    # Reshape back to a (30, 30) grid.
+    val = val.reshape(x.shape)
+
+    # Normalize max to 100 (ppmv) and transpose so orientation is correct (z up)
+    val = val.T*(100/val.max())
+
+    # Assign minimum value via ambient concentration populated in array by WRF-Chem
+    # prior to modification
+    val[val<0.03] = 0.03
+
+    print(val.shape)
+
+    profile_vals = np.ma.array(np.array([val]), mask=False, dtype='float32')
+
+    return profile_vals
+
+def checkerboard_profile(fx, fy, xgrid, ygrid, zgrid, max_val, min_val):
+    xrange, yrange = xgrid, ygrid
+    epsilon = 0.001
+    Ax, Ay = 1, 1
+    
+    #print(f'\nUsing fx={fx} and fy={fy}\n')
+    k = fx*2*np.pi/xrange #wavenumber 2pi / L
+    m = fy*2*np.pi/yrange
+    x=np.arange(xrange)
+    y=np.arange(yrange)
+    X,Y=np.meshgrid(x,y)
+    phi=Ax*np.sin(k*X+epsilon)*Ay*np.sin(m*Y+epsilon)
+
+    phi_star = phi.copy()
+
+    phi_star[phi_star > 0] = max_val
+    phi_star[phi_star <= 0] = min_val
+
+    mesh = np.zeros((xgrid, ygrid, zgrid))
+    mesh[:, :, 0] = phi_star
+
+    # transpose so z, y, x
+    mesh = mesh.T
+
+    mesh = np.ma.array(np.array([mesh]), mask=False, dtype='float32')
+
+    return mesh
